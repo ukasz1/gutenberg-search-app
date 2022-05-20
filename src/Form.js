457297403612c3069b20b85d560bb10ace31652f
ukasz1 <lukaszmitkowski53@gmail.com>
2022-05-20 15:02:33 +0200

    finding books

--- src/Form.js
+++ src/Form.js
@@ -1,8 +1,15 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { AppContext } from './context';
 
 const Form = () => {
-  const { setTitle, searchAll } = useContext(AppContext)
+  const { /*title, setTitle,*/ searchAll } = useContext(AppContext);
+  const [title, setTitle] = useState('');
+
+  const findBooks = (e) => {
+    e.preventDefault();
+    const url = `https://gnikdroy.pythonanywhere.com/api/book/?format=json&search=${title}`;
+    searchAll(url);
+  }
 
   return (
     <>
@@ -11,7 +18,7 @@ const Form = () => {
         <form>
           <label htmlFor="title">Title: </label>
           <input type="text" id="title" onChange={(e) => setTitle(e.target.value)} />
-          <button type="submit" onClick={searchAll}>Search</button>
+          <button type="submit" onClick={(e) => findBooks(e)}>Search</button>
         </form>
       </div>
     </>
