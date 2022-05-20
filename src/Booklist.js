f207b1c679087fc0a172ac25369468f60882d3e7
ukasz1 <lukaszmitkowski53@gmail.com>
2022-05-20 15:03:20 +0200

    scrolling data and clearing the code

--- src/Booklist.js
+++ src/Booklist.js
@@ -4,32 +4,52 @@ import { AppContext } from "./context";
 
 const BookList = () => {
 
-  const { data } = useContext(AppContext);
+  const { data, searchAll } = useContext(AppContext);
   const [newData, setNewData] = useState([]);
 
   useEffect(() => {
     setNewData(data.results)
   }, [data])
 
-  // console.log('newData: ', newData)
-  console.log(newData);
+  const nextData = () => {
+    if (data.next) {
+      searchAll(data.next);
+    }
+  }
+  const prevData = () => {
+    if (data.previous) {
+      searchAll(data.previous);
+    }
+  }
 
   return (
     <div className="booklist">
-      <div className="left"><button type="button">{String.fromCharCode(171)}</button></div>
+      <div className="left"><button type="button" onClick={prevData}>{String.fromCharCode(171)}</button></div>
       <div className="center">
         <table>
           <thead>
-            <th>Books<hr /></th>
+            <tr>
+              <th>Books<hr /></th>
+            </tr>
           </thead>
           <tbody>
-            {(newData != undefined) ? newData.map((item, index) => {
-              return <tr><td><div key={index}>{item.title}</div></td></tr>
-            }) : <span>Loading...</span>}
+            {(newData !== undefined) ?
+              newData.map((item, index) => {
+                return (
+                  <tr key={index}>
+                    <td>
+                      <div >
+                        <b>{item.title}</b>,
+                        <i> {item.agents[0].person}</i>
+                      </div>
+                    </td>
+                  </tr>
+                )
+              }) : (<tr><td><span>Loading...</span> </td></tr>)}
           </tbody>
         </table>
       </div>
-      <div className="right"><button type="button">{String.fromCharCode(187)}</button></div>
+      <div className="right"><button type="button" className="right-submit" onClick={nextData}>{String.fromCharCode(187)}</button></div>
     </div>
   )
 }
