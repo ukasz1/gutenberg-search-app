b5a51807e8d82ee289a64d0f4d613620117fd89d
ukasz1 <lukaszmitkowski53@gmail.com>
2022-05-24 13:54:21 +0200

    adding and deleting items from favourites+ styling

--- src/Favourites.js
+++ src/Favourites.js
@@ -1,4 +1,16 @@
+import { useContext } from 'react'
+import { AppContext } from "./context";
+
 const Favourites = () => {
+  const { favouritesTrigger, favouritesRender, handleRecord } = useContext(AppContext);
+  const storageKeys = Object.keys(localStorage);
+
+  const deleteBook = (e) => {
+    const favObject = handleRecord(e);
+    localStorage.removeItem(favObject.title + favObject.author);
+    favouritesRender(!favouritesTrigger);
+  }
+
   return (
     <div className="favourites">
       <table>
@@ -8,6 +20,19 @@ const Favourites = () => {
           </tr>
         </thead>
         <tbody>
+          {storageKeys.map((item) => {
+            const bookObject = JSON.parse(localStorage.getItem(item));
+            return (
+              <tr key={item} className="tr-content">
+                <td className="record-div">
+                  <div className='book-record' onClick={(e) => { deleteBook(e) }}>
+                    <b>{bookObject.title}</b>,{' '}
+                    <i>{bookObject.author}</i>
+                  </div>
+                </td>
+              </tr>
+            )
+          })}
         </tbody>
       </table>
     </div>
